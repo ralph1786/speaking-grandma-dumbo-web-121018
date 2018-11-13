f6157b53168695216f7d9ba37c483838f3a32044
Rafael Cruz <ralph1786@gmail.com>
2018-11-13 17:05:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
